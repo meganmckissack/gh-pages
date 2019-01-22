baf3bcc9452a57698f9cc2839a5f40320884ee45
Megan McKissack <meganmckissack@gmail.com>
2019-01-22 15:56:16 -0800

    Updates

---
+++ precache-manifest.0f64c973b0d766de0b531946d2ff9801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "61482aa6df292d9d4466",
+    "url": "/react-gh-pages/static/js/main.61482aa6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "61482aa6df292d9d4466",
+    "url": "/react-gh-pages/static/css/main.ba025882.chunk.css"
+  },
+  {
+    "revision": "7f6eb332eedfdcbd53d714386610bc49",
+    "url": "/react-gh-pages/index.html"
+  }
+];
